3a6164718c366f22a4fe29720917dfa7832c9d7e
Darian Chirca <dmc.41155@gmail.com>
2024-05-16 18:32:09 +0200

    Add new locators to project

---
+++ cypress/e2e/locators/login-locators.js
@@ -0,0 +1,11 @@
+export const loginLocators = {
+    loginForm: ".login-form",
+    signUpForm: ".signup-form",
+    emailFieldLoginForm: "[data-qa=login-email]",
+    passwordFieldLoginForm: "[data-qa=login-password]",
+    nameFieldSignUpForm: "[data-qa=signup-name]",
+    emailFieldSignUpForm: "[data-qa=signup-email]",
+    loginBtn: "[data-qa=login-button]",
+    signUpBtn: "[data-qa=signup-button]",
+    pTagLoginForm: ".login-form > form > p"
+}
